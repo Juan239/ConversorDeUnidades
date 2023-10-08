64f9b846ee08b4f4501aaf7688bf7bc1eeac71b6
Juan <juan93620672@gmail.com>
2023-10-07 22:25:58 -0300

    Fragmento longitud terminado

--- app/src/main/java/com/example/conversordeunidades/longitudFragment.java
+++ app/src/main/java/com/example/conversordeunidades/longitudFragment.java
@@ -1,64 +1,208 @@
 package com.example.conversordeunidades;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link longitudFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class longitudFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public longitudFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment longitudFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static longitudFragment newInstance(String param1, String param2) {
-        longitudFragment fragment = new longitudFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    Spinner spinnerLongitud, spinnerGrados;
+    EditText valorEntradaLongitud, valorEntradaGrados;
+    TextView resMilimetros, resCentimetros, resMetros, resKilometros, resCentesimal, resSexagesimal;
+    ScrollView scrollView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_longitud, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_longitud, container, false);
+
+        spinnerLongitud = rootView.findViewById(R.id.spinnerLongitud);
+        spinnerGrados = rootView.findViewById(R.id.spinnerGrados);
+
+        valorEntradaLongitud = rootView.findViewById(R.id.valorEntrada);
+        valorEntradaGrados = rootView.findViewById(R.id.valorEntradaGrados);
+
+        resMilimetros = rootView.findViewById(R.id.resultadoMilimetros);
+        resCentimetros = rootView.findViewById(R.id.resultadoCentimetros);
+        resMetros = rootView.findViewById(R.id.resultadoMetros);
+        resKilometros = rootView.findViewById(R.id.resultadoKilometros);
+
+        resCentesimal = rootView.findViewById(R.id.resultadoCentesimal);
+        resSexagesimal = rootView.findViewById(R.id.resultadoSexagesimal);
+
+
+        String[] valoresLongitud = {"Milímetros", "Centímetros", "Metros", "Kilometros"};
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(valoresLongitud));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.style_spinner, arrayList);
+        spinnerLongitud.setAdapter(arrayAdapter);
+
+        String[] valoresGrados = {"Centesimales", "Sexagesimales"};
+        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList(valoresGrados));
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(getActivity(), R.layout.style_spinner, arrayList2);
+        spinnerGrados.setAdapter(arrayAdapter2);
+
+        valorEntradaLongitud.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String opcion = spinnerLongitud.getSelectedItem().toString();
+                try {
+                    Float valor = Float.parseFloat(valorEntradaLongitud.getText().toString());
+                        if(opcion.equals("Milímetros")){
+                            resMilimetros.setText(escribe(igualdad(valor)));
+                            resCentimetros.setText(escribe(milimetro_a_centimetro(valor)));
+                            resMetros.setText(escribe(milimetro_a_metro(valor)));
+                            resKilometros.setText(Float.toString(milimetro_a_kilometro(valor)));
+                        } else if (opcion.equals("Centímetros")) {
+                            resMilimetros.setText(escribe(centimetro_a_milimetro(valor)));
+                            resCentimetros.setText(escribe(igualdad(valor)));
+                            resMetros.setText(escribe(centimetro_a_metro(valor)));
+                            resKilometros.setText(escribe(centimetro_a_kilometro(valor)));
+                        } else if (opcion.equals("Metros")) {
+                            resMilimetros.setText(escribe(metro_a_milimetro(valor)));
+                            resCentimetros.setText(escribe(metro_a_centimetro(valor)));
+                            resMetros.setText(escribe(igualdad(valor)));
+                            resKilometros.setText(escribe(metro_a_kilometro(valor)));
+                        } else if (opcion.equals("Kilometros")) {
+                            resMilimetros.setText(escribe(kilometro_a_milimetro(valor)));
+                            resCentimetros.setText(escribe(kilometro_a_centimetro(valor)));
+                            resMetros.setText(escribe(kilometro_a_metro(valor)));
+                            resKilometros.setText(escribe(igualdad(valor)));
+                        }
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+                return false;
+            }
+        });
+
+        valorEntradaGrados.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String opcion2 = spinnerGrados.getSelectedItem().toString();
+                try {
+                    Float valor = Float.parseFloat(valorEntradaGrados.getText().toString());
+                    if(opcion2.equals("Centesimales")){
+                        resCentesimal.setText(escribe(igualdad(valor)));
+                        resSexagesimal.setText(escribe(cen_a_sex(valor)));
+                    } else if (opcion2.equals("Sexagesimales")) {
+                        resCentesimal.setText(escribe(sex_a_cen(valor)));
+                        resSexagesimal.setText(escribe(igualdad(valor)));
+                    }
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+
+                return false;
+            }
+        });
+
+        return rootView;
+    }
+
+    //Operaciones con milimetros
+    public Float milimetro_a_centimetro(float x) {
+        float resultado = x / 10;
+        return resultado;
+    }
+
+    public Float milimetro_a_metro(float x) {
+        float resultado = x / 1000;
+        return resultado;
+    }
+
+    public Float milimetro_a_kilometro(float x) {
+        float resultado = x / 1000000;
+        return resultado;
+    }
+
+    //Operaciones con centímetros
+    public Float centimetro_a_milimetro(float x) {
+        float resultado = x * 10;
+        return resultado;
+    }
+
+    public Float centimetro_a_metro(float x) {
+        float resultado = x / 100;
+        return resultado;
+    }
+
+    public Float centimetro_a_kilometro(float x) {
+        float resultado = x / 100000;
+        return resultado;
+    }
+
+    //Operaciones con metros
+    public Float metro_a_milimetro(float x) {
+        float resultado = x / 100000;
+        return resultado;
+    }
+
+    public Float metro_a_centimetro(float x) {
+        float resultado = x * 100;
+        return resultado;
+    }
+
+    public Float metro_a_kilometro(float x) {
+        float resultado = x * 1000;
+        return resultado;
+    }
+    //Operaciones con kilometros
+    public Float kilometro_a_milimetro(float x){
+        float resultado = x * 1000000;
+        return resultado;
+    }
+    public Float kilometro_a_centimetro(float x){
+        float resultado = x * 100000;
+        return resultado;
+    }
+    public Float kilometro_a_metro(float x){
+        float resultado = x * 1000;
+        return resultado;
+    }
+    //Operaciones con grados
+    public Float cen_a_sex(float x){
+        float resultado = x*(180/200F);
+        return resultado;
+    }
+    public Float sex_a_cen(float x){
+        float resultado = x*(200/180F);
+        return resultado;
+    }
+    //Caso de igualdad
+    public Float igualdad(float x){
+        return x;
+    }
+    static String escribe(double d) {
+        NumberFormat nf = new DecimalFormat("##.####");
+        return (nf.format(d).replace(",","."));
+    }
+    public void reestablecerLongitudes(){
+        resMilimetros.setText("0");
+        resCentimetros.setText("0");
+        resMetros.setText("0");
+        resKilometros.setText("0");
+    }
+    public void reestablecerGrados(){
+        resCentesimal.setText("0");
+        resSexagesimal.setText("0");
     }
 }
